1f39871c9a02f77bbf29047ec0cc63bab7dbba41
Shodiq <shodiqmufadhol1@gmail.com>
2022-11-01 18:13:46 +0700

    commit lagi cuy

---
+++ lib/beranda.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class BerandaScreen extends StatefulWidget {
+  const BerandaScreen({super.key});
+
+  @override
+  State<BerandaScreen> createState() => _BerandaScreenState();
+}
+
+class _BerandaScreenState extends State<BerandaScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+
+      ),
+    )
+  }
+}
