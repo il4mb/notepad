acd7878016f0a88dd28605a8e0b3b8573fc140e8
ilham B <durianbohong@gmail.com>
2022-11-02 13:01:54 +0700

    sikit sikit

--- lib/kategori.dart
+++ lib/kategori.dart
@@ -1,7 +1,6 @@
-import 'dart:ffi';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import '/main.dart';
 import '/object/kategori.dart';
 
@@ -15,24 +14,52 @@ class KategoriFrame extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-    List<Widget> ListKat = <Widget>[];
-    for(var val in Kategori.defaultVal) {
-
-      ListKat.add(Text(val.value));
-    }
+    var len = Kategori.defaultVal.length;
 
     return MaterialApp(
       title: MyApp.APPNAME,
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Pilih kategori"),
-        ),
-        body: Center(
-          child: ListView(
-            children: ListKat,
+          appBar: AppBar(
+            title: const Text("Pilih kategori"),
           ),
-        ),
-      ),
+          body: Column(
+            children: [
+
+              Padding(
+                padding: EdgeInsets.all(15), //apply padding to all four sides
+                child: Text("Kamu punya $len kategori"),
+              ),
+
+              Expanded(
+                child: GridView.count(
+                  crossAxisCount: 2,
+                  children: List.generate(len, (index) {
+                    return Card(
+                        margin: EdgeInsets.all(15.0),
+                        child: Padding(
+                          padding: const EdgeInsets.all(15),
+                          child: Column(
+                            children: [
+                              const Icon(
+                                Icons.folder,
+                                color: Colors.blueGrey,
+                                size: 75.0,
+                              ),
+                              Text(Kategori.defaultVal[index].value,
+                                  style: const TextStyle(
+                                      color: Colors.blueGrey,
+                                      fontSize: 18.0,
+                                      fontWeight: FontWeight.w600)),
+                            ],
+                          ),
+                        ));
+                  }),
+                ),
+              ),
+
+            ],
+
+          )),
     );
   }
 }
